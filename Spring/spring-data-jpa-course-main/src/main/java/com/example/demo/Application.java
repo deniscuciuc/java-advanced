a40e6e0f264e12b80648ead4e7e75f573885697c
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-26 22:45:33 +0300

    Added jpa course

--- Spring/spring-data-jpa-course-main/src/main/java/com/example/demo/Application.java
+++ Spring/spring-data-jpa-course-main/src/main/java/com/example/demo/Application.java
@@ -1,13 +1,48 @@
 package com.example.demo;
 
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student(
+                    "Maria",
+                    "Heiko",
+                    "maria@gmail.com",
+                    24
+            );
+
+            Student john = new Student(
+                    "John",
+                    "Heiko",
+                    "john@gmail.com",
+                    24
+            );
+
+            Student kevin = new Student(
+                    "Kevin",
+                    "Joshua",
+                    "kevin@gmail.com",
+                    31
+            );
+
+
+            studentRepository.save(kevin);
+
+            studentRepository.deleteById(kevin.getId());
+        };
     }
 
 }

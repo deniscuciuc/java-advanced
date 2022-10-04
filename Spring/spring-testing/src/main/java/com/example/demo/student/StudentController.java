6fdc0bdc8a81e2cfd4162606f6ecc40e04d7c0cc
Denis Cuciuc <dev.dcuciuc@gmail.com>
2022-10-04 07:08:02 +0300

    Testse

--- Spring/spring-testing/src/main/java/com/example/demo/student/StudentController.java
+++ Spring/spring-testing/src/main/java/com/example/demo/student/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/students")
 @AllArgsConstructor
-public class StudentController {
+class StudentController {
 
     private final StudentService studentService;
 
